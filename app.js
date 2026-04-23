f4161ae60ac02da0f9294148018cfb348011af63
the-sann <kimsannmao025@gmail.com>
2026-04-23 17:17:08 +0700

    fix-cor

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://sala-it-express-learning-frontend.vercel.app/",
     credentials: true,
   }),
 );
